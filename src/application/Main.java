ff052d39d8ba7e26ea71669fd7976bea37b70fbb
Valentin <valentin.merault@gmail.com>
2022-03-16 20:23:19 +0100

    phase de tests : LEGOS / ENVIRONNEMENT 3D
    Création du plateau.
    
    Fonctionnalités implémentés :
    
    - Selection de différents lego.
    - Possibilité d'empiler les legos.
    - Déplacement de la camera, mouvement de la structure, lumières et réflexions.

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,8 @@
 package application;
 	
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import javafx.application.Application;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
@@ -9,6 +12,7 @@ import javafx.scene.Node;
 import javafx.scene.PerspectiveCamera;
 import javafx.scene.PointLight;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.ScrollEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
@@ -28,42 +32,95 @@ public class Main extends Application {
 	
 	private final DoubleProperty angleX = new SimpleDoubleProperty(0);
 	private final DoubleProperty angleY = new SimpleDoubleProperty(0);
+	
+	private HashMap<String, Lego> legos = new HashMap<String, Lego>();
+	
+	public String selected_bloc = "CUBE";
+	
+	public SmartGroup group;
 
+	
 	@Override
 	public void start(Stage primaryStage) {
-		SmartGroup group = new SmartGroup();
-		Box box = prepareBox(group);
-		Box box2 = new Box(30,20,200);
-		box2.setTranslateX(50);  
-		box2.setTranslateY(-20); 
 		
-		Box box3 = new Box(30,20,200);
-		box3.setTranslateX(-50);  
-		box3.setTranslateY(-20); 
+		final Group axisGroup = new Group();
+		
+		System.out.println("buildAxes()");
+        final PhongMaterial redMaterial = new PhongMaterial();
+        redMaterial.setDiffuseColor(Color.DARKRED);
+        redMaterial.setSpecularColor(Color.RED);
+ 
+        final PhongMaterial greenMaterial = new PhongMaterial();
+        greenMaterial.setDiffuseColor(Color.DARKGREEN);
+        greenMaterial.setSpecularColor(Color.GREEN);
+ 
+        final PhongMaterial blueMaterial = new PhongMaterial();
+        blueMaterial.setDiffuseColor(Color.DARKBLUE);
+        blueMaterial.setSpecularColor(Color.BLUE);
+ 
+        final Box xAxis = new Box(240.0, 1, 1);
+        final Box yAxis = new Box(1, 240.0, 1);
+        final Box zAxis = new Box(1, 1, 240.0);
+        
+        xAxis.setMaterial(redMaterial);
+        yAxis.setMaterial(greenMaterial);
+        zAxis.setMaterial(blueMaterial);
+ 
+        axisGroup.getChildren().addAll(xAxis, yAxis, zAxis);
+        
+ 
+		legos.put("CUBE",new Lego(100,100,100,"CUBE",null));
+		
+		legos.put("RECTANGLE",new Lego(200,50, 50,"RECTANGLE",null));
+		legos.put("PILLIER",new Lego(50,200, 50,"PILLIER",null));
+		legos.put("BASE",new Lego(1000,1,1000,"BASE",null));
+		
+		Lego pateforme_de_construction = legos.get("BASE");
+		
+		
+	    group = new SmartGroup();
+	    
 
-		group.getChildren().add(box);
-		group.getChildren().add(box2);
-		group.getChildren().add(box3);
+		group.getChildren().add(pateforme_de_construction);
+		
 		
 		group.getChildren().addAll(prepareLight());
 
 		Camera camera = new PerspectiveCamera();
-		Scene scene = new Scene(group, 1200, 800,  true);
+		Scene scene = new Scene(group, 1200, 800, true);
 		scene.setFill(Color.SILVER);
 		
 		scene.setCamera(camera);
-
+		group.getChildren().addAll(axisGroup);
 		group.translateXProperty().set(1200/2);
 		group.translateYProperty().set(800/2);
-		group.translateZProperty().set(-1000);
+		group.translateZProperty().set(0);
 		
 		initMouseControl(group, scene, primaryStage);
 
 		primaryStage.setTitle("gerogr");
 		primaryStage.setScene(scene);
 		primaryStage.show();
-	}
-	
+		
+		primaryStage.addEventHandler(KeyEvent.KEY_PRESSED, event ->{
+			switch(event.getCode()) {
+			
+			case W:
+				selected_bloc = "RECTANGLE";
+				System.out.println("RECTANGLE");
+				break;
+			case E:
+				selected_bloc = "CUBE";
+				break;
+			case P:
+				selected_bloc = "PILLIER";
+				break;
+			}
+			
+			
+		});	
+}
+
 	public static void main(String[] args) {
 		launch(args);
 	}
@@ -97,7 +154,6 @@ public class Main extends Application {
 		
 		
 		
-		
 		stage.addEventHandler(ScrollEvent.SCROLL, event ->{
 			double delta = event.getDeltaY();
 			group.translateZProperty().set(group.getTranslateZ() + delta);
@@ -124,33 +180,7 @@ public class Main extends Application {
 		
 	}
 	
-	private Box prepareBox(SmartGroup group) {
-		PhongMaterial material = new PhongMaterial();
-		material.setDiffuseColor(Color.ROYALBLUE);
-		material.setSpecularColor(Color.valueOf("#424242"));
-		Box box = new Box(100,20,50);
-		box.setMaterial(material);
-		box.setTranslateX(0);  
-		box.setTranslateY(0);  
-		
-		box.setOnMouseClicked(event ->{
-			
-			System.out.println(event.getTarget().toString());
-			System.out.println(event.getX());
-			System.out.println(event.getY());
-	
-			Box box2 = new Box(50,50,50);
-			box.setMaterial(material);
-			
-			box.setTranslateX(event.getX());  
-			box.setTranslateY(event.getY());
-			box.setTranslateZ(event.getZ());  
-			group.getChildren().add(box2);
 
-		});
-			
-		return box;
-	}
 	
 	class SmartGroup extends Group{
 		Rotate r;
@@ -169,6 +199,102 @@ public class Main extends Application {
 			this.getTransforms().clear();
 			this.getTransforms().addAll(t);
 		}
+		
+	}
+	
+	
+
+	class Lego extends Box  {
+		
+		Lego parent;
+		Lego enfant;
+		
+		public int width;
+		public int height;
+		public int depth;
+		
+		public String type;
+		public Color color = Color.AQUAMARINE;
+		
+		public Lego(int width, int height , int depth,String type, Lego parent) {
+			
+			this.width = width;
+			this.height = height;
+			this.depth = depth;
+			this.parent =parent;
+			this.type = type;
+			PhongMaterial material = new PhongMaterial();
+			material.setDiffuseColor(Color.ROYALBLUE);
+			material.setSpecularColor(Color.valueOf("#424242"));
+			this.setMaterial(material);
+			this.setWidth(width);
+			this.setHeight(height);
+			this.setDepth(depth);
+			
+			
+			this.setOnMouseClicked(event ->{
+				
+				
+				if(this.type.equals("BASE")) {
+					Lego new_lego = new Lego(legos.get(selected_bloc).width, legos.get(selected_bloc).height, legos.get(selected_bloc).depth,selected_bloc, this);
+					
+					new_lego.setTranslateX( event.getX());
+					new_lego.setTranslateY( -1 * (legos.get(selected_bloc).height / 2));
+					new_lego.setTranslateZ( event.getZ());
+					
+					System.out.println(new_lego.parent);
+					
+					
+					group.getChildren().add(new_lego);
+				}else {
+					Lego new_lego = new Lego(legos.get(selected_bloc).width, legos.get(selected_bloc).height, legos.get(selected_bloc).depth,selected_bloc, this);
+					
+					new_lego.setTranslateX(  this.getTranslateX());
+					new_lego.setTranslateY( -1 * (this.getTotalHeight()));
+					new_lego.setTranslateZ( this.getTranslateZ());
+					
+					System.out.println(new_lego.parent);
+					
+					
+					group.getChildren().add(new_lego);
+				}
+				
+				
+			});
+			
+		}
+		
+		public void addChild(Lego child) {
+			this.enfant = child;
+		}
+		
+		
+		public int getTotalHeight() {
+			
+			int height = this.height;
+
+			
+			Lego curr = this;
+			
+			
+				while(curr.parent != null) {
+					
+					if(curr.parent.type != "BASE") {
+						height += curr.parent.height;
+						System.out.println("HAUTEUR PAPA :"+curr.parent.height);
+
+				
+					}
+					
+					curr = curr.parent;
+					
+				}
+			
+			
+			System.out.println("HAUTEUR TOTALE :"+ height);
+			return height;
+		}
+		
 		
 	}
 }
