fb6774e4c869836af6ec3423986a7bc3e73c5b9d
Valentin <valentin.merault@gmail.com>
2022-03-22 20:59:48 +0100

    Tentative de rotation automatique de la structure.

--- src/Controllers/Controller_3D_Environnement.java
+++ src/Controllers/Controller_3D_Environnement.java
@@ -5,6 +5,7 @@ import java.util.Deque;
 
 import Models.Legos_collection;
 import Models.Structure_3D;
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
@@ -33,20 +34,24 @@ public class Controller_3D_Environnement extends Application {
 	private final DoubleProperty angleX = new SimpleDoubleProperty(0);
 	private final DoubleProperty angleY = new SimpleDoubleProperty(0);
 
+	private Camera camera;
+	private Camera firstPersoncamera;
+
 	public Structure_3D structure;
 
 	private Legos_collection leg;
-	
+
 	int couleur = 0;
-	
-	Color[] tab_couleur = {Color.ROYALBLUE,Color.GREEN, Color.SANDYBROWN, Color.IVORY, Color.AQUA};
-	
+
+	Color[] tab_couleur = { Color.ROYALBLUE, Color.GREEN, Color.SANDYBROWN, Color.IVORY, Color.AQUA };
+
 	int rota = 0;
-		
-	String[] rotations = {"DROITE", "GAUCHE", "AVANT", "ARRIERE"};
-	
+
+	boolean r = false;
+	String[] rotations = { "DROITE", "GAUCHE", "AVANT", "ARRIERE" };
+
 	String bloc = "RECTANGLE2_" + this.rotations[this.rota];
-	
+
 	public Controller_3D_Environnement(Structure_3D structure) {
 		this.structure = structure;
 	}
@@ -57,64 +62,84 @@ public class Controller_3D_Environnement extends Application {
 		structure.createBase();
 		structure.getChildren().addAll(prepareLight());
 
-		Camera camera = new PerspectiveCamera();
-		
+		camera = new PerspectiveCamera();
+
+		/*
+		 * firstPersoncamera = new PerspectiveCamera(true);
+		 * firstPersoncamera.getTransforms().addAll(new Translate(0,800, -100), new
+		 * Rotate(90, Rotate.X_AXIS)); firstPersoncamera.setFarClip(10);
+		 * firstPersoncamera.setNearClip(0);
+		 * 
+		 * Camera pour minecraft MODE
+		 * 
+		 */
 		Scene scene = new Scene(structure, 1200, 800, true);
 
 		scene.setFill(Color.valueOf("#2C2D32"));
 
 		scene.setCamera(camera);
-		structure.translateXProperty().set(1200/2);
+		structure.translateXProperty().set(1200 / 2);
 		structure.translateYProperty().set(1000 / 2);
 		structure.translateZProperty().set(0);
 
 		initMouseControl(structure, scene, primaryStage);
 
-		primaryStage.setTitle("gerogr");
+		primaryStage.setTitle("LEGOLAND");
 		primaryStage.setScene(scene);
 		primaryStage.show();
 
 		primaryStage.addEventHandler(KeyEvent.KEY_PRESSED, event -> {
 
 			switch (event.getCode()) {
+
 			/*
-			case L:
-				structure.selected_color = Color.GREEN;
+			 * case L: structure.selected_color = Color.GREEN; break; case J:
+			 * structure.selected_color = Color.SANDYBROWN; break; case K:
+			 * structure.selected_color = Color.IVORY; break; case S:
+			 * structure.selected_color = Color.AQUA; break; case T: structure.selected_bloc
+			 * = "RECTANGLE2_DROITE"; break; case I: structure.selected_bloc =
+			 * "RECTANGLE4_AVANT"; break; case O: structure.selected_bloc =
+			 * "RECTANGLE4_ARRIERE"; break; case Y: structure.selected_bloc =
+			 * "RECTANGLE2_GAUCHE"; break;
+			 * 
+			 * case A: structure.selected_bloc = "RECTANGLE4_DROITE"; break; case E:
+			 * structure.selected_bloc = "CUBE"; break;
+			 * 
+			 * case M: structure.selected_bloc = "RECTANGLE4_GAUCHE"; break;
+			 */
+
+			/*
+			 * touche minecraft case Z: System.out.println("AVANT");
+			 * firstPersoncamera.setTranslateY(firstPersoncamera.getTranslateY() - 20);
+			 * 
+			 * break; case S: System.out.println("arriere");
+			 * firstPersoncamera.setTranslateY(firstPersoncamera.getTranslateY() + 20);
+			 * 
+			 * break; case A: System.out.println("gauche");
+			 * firstPersoncamera.setTranslateX(firstPersoncamera.getTranslateX() - 20);
+			 * 
+			 * break; case D: System.out.println("droite");
+			 * firstPersoncamera.setTranslateX(firstPersoncamera.getTranslateX() + 20);
+			 * 
+			 * break;
+			 */
+			case R:
+
+				if (r == false) {
+
+					structure.setRotationAxis(Rotate.Y_AXIS);
+					r = true;
+					spinAnimation();
+				} else {
+					structure.getTransforms().addAll(new Rotate(0, Rotate.X_AXIS), new Rotate(0, Rotate.Y_AXIS)
+
+					);
+					r = false;
+					spinAnimation();
+				}
+
 				break;
-			case J:
-				structure.selected_color = Color.SANDYBROWN;
-				break;
-			case K:
-				structure.selected_color = Color.IVORY;
-				break;
-			case S:
-				structure.selected_color = Color.AQUA;
-				break;
-			case T:
-				structure.selected_bloc = "RECTANGLE2_DROITE";
-				break;
-			case I:
-				structure.selected_bloc = "RECTANGLE4_AVANT";
-				break;
-			case O:
-				structure.selected_bloc = "RECTANGLE4_ARRIERE";
-				break;
-			case Y:
-				structure.selected_bloc = "RECTANGLE2_GAUCHE";
-				break;
-				
-			case A:
-				structure.selected_bloc = "RECTANGLE4_DROITE";
-				break;
-			case E:
-				structure.selected_bloc = "CUBE";
-				break;
-		
-			case M:
-				structure.selected_bloc = "RECTANGLE4_GAUCHE";
-				break;
-			*/
-			
+
 			case U:
 				structure.selected_bloc = "CUBE";
 				break;
@@ -131,48 +156,45 @@ public class Controller_3D_Environnement extends Application {
 				String bloc3 = "RECTANGLE4_" + this.rotations[this.rota];
 				structure.selected_bloc = bloc3;
 				break;
-				
+
 			case B:
 				structure.deleteLastBloc();
 				break;
-				
+
 			case N:
 				structure.recupDeletedBloc();
 				break;
-				
+
 			case C:
-			
-				if(this.couleur == this.tab_couleur.length-1) {
+
+				if (this.couleur == this.tab_couleur.length - 1) {
 					this.couleur = 0;
-				}else {
+				} else {
 					this.couleur += 1;
-					
+
 				}
-				
+
 				structure.selected_color = this.tab_couleur[this.couleur];
 				break;
-				
+
 			case V:
-				
-				if(structure.selected_bloc != "CUBE") {
-					if(this.rota == this.rotations.length-1) {
+
+				if (structure.selected_bloc != "CUBE") {
+					if (this.rota == this.rotations.length - 1) {
 						this.rota = 0;
-					}else {
+					} else {
 						this.rota += 1;
-						
+
 					}
 					String[] blocs = structure.selected_bloc.split("_");
-					
-					structure.selected_bloc = blocs[0] + "_" +this.rotations[rota];
+
+					structure.selected_bloc = blocs[0] + "_" + this.rotations[rota];
 				}
-				
+
 				break;
-			
+
 			}
-			
-			
-			
-		
+
 		});
 
 	}
@@ -187,9 +209,9 @@ public class Controller_3D_Environnement extends Application {
 
 		xRotate.angleProperty().bind(angleX);
 		yRotate.angleProperty().bind(angleY);
-		
+
 		scene.setOnMousePressed(event -> {
-			
+
 			anchorX = event.getSceneX();
 			anchorY = event.getSceneY();
 			anchorAngleX = angleX.get();
@@ -198,11 +220,10 @@ public class Controller_3D_Environnement extends Application {
 		});
 
 		scene.setOnMouseDragged(event -> {
-			if(event.isPrimaryButtonDown()) {
+			if (event.isPrimaryButtonDown()) {
 				angleX.set(anchorAngleX - (anchorY - event.getSceneY()));
 				angleY.set(anchorAngleY - (anchorX - event.getSceneX()));
 			}
-			
 
 		});
 
@@ -212,8 +233,31 @@ public class Controller_3D_Environnement extends Application {
 		});
 
 	}
-	
-	
+
+	private void spinAnimation() {
+
+		AnimationTimer timer = new AnimationTimer() {
+
+			@Override
+			public void handle(long arg0) {
+				if (r == true) {
+
+					structure.rotateProperty().set(structure.getRotate() + 0.2);
+				} else {
+					structure.rotateProperty().set(structure.getRotate());
+
+				}
+			}
+
+		};
+		if (r == true) {
+			timer.start();
+
+		} else {
+			timer.stop();;
+		}
+
+	}
 
 	private Node[] prepareLight() {
 		PointLight pointLight = new PointLight();
