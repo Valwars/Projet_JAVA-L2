ff9e7bf7a3466c0f0ff059080cf9bf92ed4101a7
Valentin <valentin.merault@gmail.com>
2022-05-12 21:02:58 +0200

    Résolution bug onglet fermeture dernier

--- src/Views/Main.java
+++ src/Views/Main.java
@@ -100,47 +100,57 @@ public class Main extends Application{
 			                
 				            }else {
 				            	
-				            	SingleSelectionModel<Tab> selectionModel2 = tp.getSelectionModel();
-			                	
-			                	selectionModel2.select(tp.getTabs().get(0)); //select by object
-			            	
-			                	String[] dos = new String[file.list().length];
-			                	
-			                	String[] verif = file.list();
-			                	
-			                	dos[0] = "Nouvelle structure";
-			                	
-			                	for(int i = 1;i<file.list().length;i++) {
-				                	dos[i]=verif[i].substring(0,verif[i].length()-4);
-			                	}
-			                	
-				            	ChoiceDialog dialog = new ChoiceDialog("Nouvelle structure",dos);
-				            	
-				            	dialog.getDialogPane().setPrefWidth(500);
-
-
-				            	dialog.setTitle("Nouvelle/charger structure");
-                                dialog.setHeaderText("Choix en cours...");
-
-                                Optional<String> result = dialog.showAndWait();
-                             
-                                
-				            	if (result.get()=="Nouvelle structure") {
-				            	
+				            	if(tp.getTabs().size() <= 1) {
 					            	create_newTab(tp, legos_collection,primaryStage,null);
+					            	
+					            }else {
+					            
+					            	SingleSelectionModel<Tab> selectionModel2 = tp.getSelectionModel();
+				                	
+				                	selectionModel2.select(tp.getTabs().get(0)); //select by object
+				            	
+				                	String[] dos = new String[file.list().length];
+				                	
+				                	String[] verif = file.list();
+				                	
+				                	dos[0] = "Nouvelle structure";
+				                	
+				                	for(int i = 1;i<file.list().length;i++) {
+					                	dos[i]=verif[i].substring(0,verif[i].length()-4);
+				                	}
+				                	
+					            	ChoiceDialog dialog = new ChoiceDialog("Nouvelle structure",dos);
+					            	
+					            	dialog.getDialogPane().setPrefWidth(500);
 
-				            		
-				            	}else{
-				            		
-					            	create_newTab(tp, legos_collection,primaryStage,result.get());
 
-				            	}
+					            	dialog.setTitle("Nouvelle/charger structure");
+	                                dialog.setHeaderText("Choix en cours...");
+
+	                                Optional<String> result = dialog.showAndWait();
+	                             
+	                                
+					            	if (result.get()=="Nouvelle structure") {
+					            	
+						            	create_newTab(tp, legos_collection,primaryStage,null);
+
+					            		
+					            	}else{
+					            		
+						            	create_newTab(tp, legos_collection,primaryStage,result.get());
+
+					            	}
+					            }
+				            	
 				            	
 				            }
 				        }
 
 	                	
-	            }}
+	            }
+	                
+	            
+	            }
 	            });
 	       
 			
