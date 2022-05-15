f9d86a0aee1c862f51ce7c56ba3222f10a4d3fcc
Amauryyyyy <95430212+Amauryyyyy@users.noreply.github.com>
2022-05-15 16:12:04 +0200

    toggle switch couleur

--- src/Models/ToggleSwitch.java
+++ src/Models/ToggleSwitch.java
@@ -10,11 +10,13 @@ import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.scene.Parent;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Arc;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 import javafx.util.Duration;
 
 public class ToggleSwitch extends Parent {
+	public int a = 0;
 	
 	private BooleanProperty swichedOn = new SimpleBooleanProperty(false);
 	
@@ -47,12 +49,25 @@ public class ToggleSwitch extends Parent {
 			boolean isOn = newState.booleanValue();
 			translateAnimation.setToX(isOn ? 50 - 25 : 0);
 			fillAnimation.setFromValue(isOn ? Color.WHITE : Color.GRAY);
-			fillAnimation.setToValue(isOn ? Color.GRAY : Color.WHITE);
+			fillAnimation.setToValue(isOn ? Color.valueOf("#98a3ce"): Color.WHITE);
 			animation.play();
 		});
 		
 		setOnMouseClicked(event ->{
 			swichedOn.set(!swichedOn.get());
+			if (a==0) {
+				background.setStroke(Color.BLACK);
+				trigger.setStroke(Color.BLACK);
+				a+=1;
+				
+			}
+			else {
+				background.setStroke(Color.LIGHTGRAY);
+				trigger.setStroke(Color.LIGHTGRAY);
+				a-=1;
+			}
+
+			
 		});
 	}
 	public BooleanProperty select() {
